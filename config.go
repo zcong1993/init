6611bb63837ec8b29e615872c52a78fd31a3a879
zcong1993 <1005997919@qq.com>
2017-06-18 22:06:03 +0800

    parser config

--- config.go
+++ config.go
@@ -1,13 +1,55 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"github.com/tj/go-prompt"
 	"github.com/buger/jsonparser"
-	"fmt"
 )
 
-func parse(data []byte) {
-	jsonparser.ObjectEach(data, func(key []byte, value []byte, dataType jsonparser.ValueType, offset int) error {
-		fmt.Printf("Key: '%s'\n Value: '%s'\n Type: %s\n", string(key), string(value), dataType)
-		return nil
-	}, "prompts")
+type Config struct {
+	Prompts map[string]Prompt
+}
+
+type Prompt struct {
+	Message string
+	Type string
+	Default string
+}
+
+func GetConfig(configPath string) (map[string]interface{}, error)  {
+	rawConfig, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return nil, err
+	}
+	var config Config
+	err = json.Unmarshal(rawConfig, &config)
+	if err != nil {
+		return nil, err
+	}
+	res := map[string]interface{}{}
+	for key, val := range config.Prompts {
+		if val.Type == "confirm" {
+			res[key] = prompt.Confirm(val.Message + " y/n ")
+		}
+		if val.Type == "string" {
+			if val.Default == "" {
+				res[key] = prompt.StringRequired(val.Message + " : ")
+			} else {
+				res[key] = prompt.String(val.Message + "(Default is %s) : ", val.Default)
+				if res[key] == "" {
+					res[key] = val.Default
+				}
+			}
+		}
+		if val.Type == "list" {
+			list := []string{}
+			jsonparser.ArrayEach(rawConfig, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
+				list = append(list, string(value))
+			}, "prompts", key, "choices")
+			i := prompt.Choose(val.Message, list)
+			res[key] = list[i]
+		}
+	}
+	return res, nil
 }
